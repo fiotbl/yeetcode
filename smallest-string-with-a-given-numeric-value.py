16d08ae52c69e962d19f5047eff5e126a8690284
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2022-10-25 21:47:57 +0200

    Create smallest-string-with-a-given-numeric-value.py
    smallest-string-with-a-given-numeric-value.py

---
+++ smallest-string-with-a-given-numeric-value.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getSmallestString(self, n: int, k: int) -> str:
+        alphaDict = dict(zip(range(1,27), string.ascii_lowercase))
+        currK = k   
+        maxValue = 26
+        res = ""
+        while(n > 0):
+            if currK == n:
+                res = currK*"a" + res
+                break
+            if (currK - maxValue) < (n-1):
+                maxValue -= 1
+                continue
+            res = alphaDict[maxValue] + res
+            n -= 1
+            currK = currK - maxValue
+            maxValue = 26
+        return res
