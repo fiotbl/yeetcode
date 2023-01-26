bc652f9d2a09abbf86ba6424220a494f42f1e045
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-01-26 21:36:36 +0800

    Time: 38 ms (78.59%), Space: 13.9 MB (73.14%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,23 +5,83 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        ansHead = movingHead = ListNode()
+        ansHead = currHead = ListNode()
         
         while list1 and list2:
             if list1.val <= list2.val:
-                movingHead.next= list1
+                currHead.next = list1
                 list1 = list1.next
-            elif list1.val > list2.val:
-                movingHead.next= list2
+            elif list2.val < list1.val:
+                currHead.next = list2
                 list2 = list2.next
-            movingHead = movingHead.next
-        
-        if list1:
-            movingHead.next = list1
+            currHead = currHead.next
             
+        if list1:
+            currHead.next = list1
+        
         if list2:
-            movingHead.next = list2
+            currHead.next = list2
         
         return ansHead.next
+                   
+                  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ansHead = movingHead = ListNode()
+        
+#         while list1 and list2:
+#             if list1.val <= list2.val:
+#                 movingHead.next= list1
+#                 list1 = list1.next
+#             elif list1.val > list2.val:
+#                 movingHead.next= list2
+#                 list2 = list2.next
+#             movingHead = movingHead.next
+        
+#         if list1:
+#             movingHead.next = list1
+            
+#         if list2:
+#             movingHead.next = list2
+        
+#         return ansHead.next
                   
                 
