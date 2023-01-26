f16ed8d8c08c9fa2ad5b5c96ec7209936195312c
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-01-26 19:06:11 +0800

    Time: 111 ms (56.27%), Space: 15.2 MB (20.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,6 +3,56 @@ class Solution:
         d = { num : index for index, num in enumerate(nums)}
         
         for i in range(len(nums)):
-            remainder = target - nums[i]
-            if remainder in d and d[remainder]!=i:
-                return [i, d[remainder]]
+            diff = target - nums[i]
+            if diff in d and i != d[diff]:
+                return [i, d[diff]]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         d = { num : index for index, num in enumerate(nums)}
+        
+#         for i in range(len(nums)):
+#             remainder = target - nums[i]
+#             if remainder in d and d[remainder]!=i:
+#                 return [i, d[remainder]]
