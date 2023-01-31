22ea93ae7578e368560d5244385d97ee2368650b
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-01-31 22:02:43 +0800

    Time: 170 ms (75.38%), Space: 15.5 MB (81.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,10 +3,8 @@ class Solution:
         count, res = 0, 0
         
         for n in nums:
-            if n!= res and count > 0: 
-                count -= 1
-            elif n != res and count == 0: 
+            if count == 0:
                 res = n
-            else: count += 1
+            count += 1 if n == res else -1
                 
         return res
