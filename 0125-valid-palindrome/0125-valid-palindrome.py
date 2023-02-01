2fa8549f7bb31fe6df1f3e447748e72603fccd50
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-02-01 19:53:43 +0800

    Time: 56 ms (52.85%), Space: 19.3 MB (8.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        news = []
-        
-        for letter in s:
-            if letter.isalnum():
-                news.append(letter.lower())
-              
-        print(news)
+
+        news = "".join(ch.lower() for ch in s if ch.isalnum())
         l, r = 0, len(news)-1
-        
+
         while l<=r:
             if news[l]!=news[r]: return False
             l, r = l+1, r-1
