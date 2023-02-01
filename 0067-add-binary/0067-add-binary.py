584be5887d61c850413d1643ce36f95bd913c0d5
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-02-01 20:56:51 +0800

    Time: 27 ms (96.46%), Space: 13.8 MB (97.72%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -3,13 +3,13 @@ class Solution:
         if len(a) > len(b): b = "0"*(len(a)-len(b)) + b
         else: a = "0"*(len(b)-len(a)) + a
         
-        res, carry = [], 0
+        res, carry = "", 0
 
         for i in range(len(a) - 1, -1, -1):
             bitsum = int(a[i]) + int(b[i]) + carry
-            res.append(bitsum % 2)
+            res += str(bitsum % 2)
             if bitsum >=2: carry = 1
             else: carry = 0
                 
-        if carry==1: res.append(1) 
-        return "".join(str(a) for a in res[::-1] )
+        if carry==1: res += "1"
+        return res[::-1]
