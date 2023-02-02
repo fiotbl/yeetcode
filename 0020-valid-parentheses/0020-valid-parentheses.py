3c729d47f12a58b9ec5404bbb959250bdeeabb37
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-02-02 21:04:23 +0800

    Time: 22 ms (98.98%), Space: 13.9 MB (64.27%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,77 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        bracketDict = { "(" : ")",
-               "{" : "}",
-               "[" : "]"}
-        
+        dictionary = {"(":")",
+                     "{":"}",
+                     "[":"]"}
         stack = []
-
+        
         for i in s:
-            if i in bracketDict:
-                stack.append(bracketDict[i])
-            elif len(stack) == 0 or stack[-1] != i:
+            if i in dictionary:
+                stack.append(dictionary[i])
+            elif len(stack)==0 or i != stack[len(stack)-1]:
                 return False
             else:
                 stack.pop()
         
-        return len(stack) == 0
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         d = { "(" : ")",
-#             "[" : "]",
-#             "{": "}"}
-#         stack = []
-        
-#         for i in s:
-#             if i in d:
-#                 stack.append(d[i])
-#             elif not stack or stack[-1]!=i:
-#                 return False
-#             else:
-#                 stack.pop()
+        return len(stack)==0
                 
-#         return len(stack)==0
