37e950dde405a66587037d2263ea2509bda0f819
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-02-06 22:22:24 +0800

    Time: 539 ms (97.09%), Space: 16.5 MB (97.90%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,20 +1,17 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        rows = len(mat)
-        cols = len(mat[0])
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j]!=0:
+                    up = mat[i-1][j] if i>0 else float('inf')
+                    left = mat[i][j-1] if j>0 else float('inf')         
+                    mat[i][j] = min(up, left) + 1
+                    
+        for i in range(len(mat)-1, -1, -1):
+            for j in range(len(mat[0])-1, -1, -1):
+                if mat[i][j]!=0:
+                    down = mat[i+1][j] if i<len(mat)-1 else float('inf')
+                    right = mat[i][j+1] if j<len(mat[0])-1 else float('inf')         
+                    mat[i][j] = min(mat[i][j], min(down, right) + 1)
         
-        for i in range(rows):
-            for j in range(cols):
-                if mat[i][j] != 0:
-                    left = mat[i][j-1] if j > 0 else float('inf')
-                    up = mat[i-1][j] if i > 0 else float('inf')       
-                    mat[i][j] = min(left, up) + 1
-                    
-        for i in range(rows-1,-1,-1):
-            for j in range(cols-1,-1,-1):
-                if mat[i][j] != 0:
-                    right = mat[i][j+1] if j < cols-1 else float('inf')
-                    down = mat[i+1][j] if i < rows-1 else float('inf')       
-                    mat[i][j] = min(min(right, down)+1, mat[i][j])
-                    
         return mat
