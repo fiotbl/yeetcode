96d22df2614b7d6295fd68b1bdced2eb750dbf35
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-02-06 23:11:14 +0800

    Time: 826 ms (72.12%), Space: 20.3 MB (59.90%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,19 +1,17 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        distances = {}
+        heap = []
+        
+        for x, y in points:
+            dist = x**2 + y**2
+            heap.append([dist, x, y])
+        
+        heapq.heapify(heap)
         res = []
         
-        for i, point in enumerate(points):
-            dist = ((point[0] - 0)**2 + (point[1] - 0)**2 )**0.5
-            distances[i] = dist
-        
-        sortedDistances = sorted(distances.items(), key=lambda x: x[1])
-
-        for key in sortedDistances:
-            if k == 0: break
-            res.append(points[key[0]])
+        while k>0:
+            _, x, y = heapq.heappop(heap)
+            res.append([x,y])
             k-=1
-            
         return res
-        
-        
+            
