9317bba7c39a45031be4509447b36d4d2059a9d2
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-02-15 23:23:14 +0800

    Time: 52 ms (94.68%), Space: 14.1 MB (44.56%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,7 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         uniqueSet = set()
-        res = 0
+        res, left = 0, 0
         left = 0
         
         for letter in s:
@@ -9,7 +9,6 @@ class Solution:
                 uniqueSet.remove(s[left])
                 left += 1
             uniqueSet.add(letter)
-            print(len(uniqueSet))
             res = max(res, len(uniqueSet))
             
         return res
