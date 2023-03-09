fecf8e2e48b5cc9199df43efaab08a4ef22f9f34
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-03-09 22:28:23 +0800

    Time: 39 ms (38.61%), Space: 13.9 MB (62.59%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,17 +1,17 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if len(a)<len(b):
-            a = "0"*(len(b)-len(a))+a
+        if len(a) < len(b):
+            a = "0"*(len(b) - len(a)) + a
         else:
-            b = "0"*(len(a)-len(b))+b
+            b = "0"*(len(a) - len(b)) + b
             
         carry = 0
         res = ""
-        
-        for i in range(len(a)-1, -1, -1):
-            total = carry + int(a[i]) + int(b[i])
-            carry = total // 2
-            res =  str(total % 2) + res
+        for i in range(len(a)-1,-1,-1):
+            addition = carry + int(a[i]) + int(b[i])
+            ans = addition % 2
+            carry = addition // 2
+            res = str(ans) + str(res)
             
-        if carry == 1: res = "1"+res 
-        return res
+        return res if carry == 0 else "1"+res
+            
