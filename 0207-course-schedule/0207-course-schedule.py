47b488d4455f3bc4b43491fb03ec6a74c7f71b16
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-03-15 21:51:25 +0800

    Time: 145 ms (13.98%), Space: 17.1 MB (36.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -8,13 +8,19 @@ class Solution:
         visitedCourses = []
         
         def dfs(course):
+            print("course", course)
             if course in visitedCourses: return False
-            if preMap[course] == []: return True
+            if preMap[course] == []: 
+                print("empty prereqs")
+                return True
             
             visitedCourses.append(course)
             for crs in preMap[course]:
+                print(course, "has a prereq of" , crs)
                 if not dfs(crs): return False
-            
+            print("course", course)
+                
+            print("checked all its decendents")
             visitedCourses.remove(course)
             preMap[course] = []
             
