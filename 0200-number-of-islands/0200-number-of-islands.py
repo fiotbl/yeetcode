75a12ff04bbf74c0b3f55d2cbed29ee0bd7c5ca0
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-03-18 23:08:31 +0800

    Time: 351 ms (36.20%), Space: 25.1 MB (11.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,20 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
        
-        def dfs(grid, i, j, visited):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0' or (i,j) in visited:
-                return
-            visited.add((i,j))
-            dfs(grid, i+1, j, visited)
-            dfs(grid, i-1, j, visited)
-            dfs(grid, i, j+1, visited)
-            dfs(grid, i, j-1, visited)
+        def dfs(grid, i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == "0" or (i,j) in visitedSet: return 
 
-        num_islands = 0
-        visited = set()
+            visitedSet.add((i,j))
+
+            dfs(grid, i - 1, j)
+            dfs(grid, i + 1, j)
+            dfs(grid, i, j - 1)
+            dfs(grid, i, j + 1)
+
+        islands = 0
+        visitedSet = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1' and (i,j) not in visited:
-                    dfs(grid, i, j, visited)
-                    num_islands += 1
-        return num_islands
+                if grid[i][j] == "1" and (i,j) not in visitedSet:
+                    dfs(grid, i, j)
+                    islands += 1
+
+        return islands
+    
