00231a80bf7be4160e626c0f601477a661badfe4
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-03-19 16:45:31 +0800

    Time: 70 ms (44.70%), Space: 14 MB (57.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,15 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp = [[0] * (target+1) for _ in range(len(candidates)+1)]
-        for i in range(len(candidates)+1):
-            dp[i][0] = 1
-        for i in range(1, len(candidates)+1):
-            for j in range(1, target+1):
-                if j < candidates[i-1]:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i][j-candidates[i-1]] + dp[i-1][j]
-        res = []
-        def backtrack(path, target, i):
+
+        def dfs(candidates, target, start, path, res):
+            if target < 0:
+                return
             if target == 0:
                 res.append(path)
-            elif target > 0:
-                for j in range(i, len(candidates)):
-                    backtrack(path + [candidates[j]], target - candidates[j], j)
-        backtrack([], target, 0)
+                return
+            for i in range(start, len(candidates)):
+                dfs(candidates, target - candidates[i], i, path + [candidates[i]], res)
+        res = []
+        candidates.sort()
+        dfs(candidates, target, 0, [], res)
         return res
