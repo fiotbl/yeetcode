cf9d0117061ad852bab653ad07e933656a05b5a5
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-03-19 15:24:29 +0800

    Time: 71 ms (12.01%), Space: 14.1 MB (8.29%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,32 +1,34 @@
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        
+    def orangesRotting(self, grid: List[List[int]]) -> int:        
         rotten = []
         count = 0
-        
-        def rot(grid, i, j):
-            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] == 0  or grid[i][j] == 2: return
-            grid[i][j] = 2
-            if (i,j) not in rotten:
-                rotten.append((i,j))      
+        fresh = 0
             
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     rotten.append((i,j))
-        
+                elif grid[i][j] == 1:
+                    fresh += 1
+                    
+        directions = [[0, 1], [0, -1], [1,0], [-1,0]]
         while rotten:
             count += 1
+            print(rotten)
             rottenLen = len(rotten)
             for i in range(rottenLen):
-                cell = rotten.pop(0)
-                rot(grid, cell[0]-1, cell[1])
-                rot(grid, cell[0]+1, cell[1])
-                rot(grid, cell[0], cell[1]-1)
-                rot(grid, cell[0], cell[1]+1)
+                row, col = rotten.pop(0)
+                for dr, dc in directions:
+                    r, c = dr+row, dc+col
+                    print(r,c)
+                    if r<0 or r>=len(grid) or c<0 or c>=len(grid[0]) or grid[r][c] == 0  or grid[r][c] == 2: continue
+                    grid[r][c] = 2
+                    rotten.append((r,c))      
+                    fresh -= 1
+                    print(fresh)
+                    
             
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    return -1
+        if fresh > 0: 
+            print("erg")
+            return -1
         return count-1 if count>0 else 0
