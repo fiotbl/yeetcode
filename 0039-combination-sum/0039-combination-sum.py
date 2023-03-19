664b89c2176fa5172c5ae0e933cc8fabddb9f92b
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-03-19 20:48:27 +0800

    Time: 67 ms (53.57%), Space: 14 MB (57.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,16 +3,16 @@ class Solution:
         res = []
         
         def dfs(i, path, total):
-            if total == target:
+            if total > target or i >= len(candidates): return
+            if total == target: 
                 res.append(path.copy())
                 return
-            if total > target or i>=len(candidates): return
             
             path.append(candidates[i])
-            dfs(i, path, total + candidates[i])
+            dfs(i, path, total+candidates[i])
             path.pop()
             dfs(i+1, path, total)
             
-        dfs(0, [], 0)
-        
+        dfs(0,[],0)
         return res
+            
