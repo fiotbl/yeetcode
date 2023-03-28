b6144725790cc9d0e0f2a121acf91fa751e777c4
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-03-28 21:27:07 +0800

    Time: 9422 ms (9.75%), Space: 13.9 MB (44.47%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,23 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        visited = set()
+        def dfs(i, j, curString, m):
+            # print(curString)
+            if curString == word: return True
+            if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or (i,j) in visited or board[i][j] != word[m]: return False
+            if board[i][j] == word[m]:
+                visited.add((i,j))
+                m += 1
+                curString += (board[i][j])
+                if dfs(i-1, j, curString, m) or dfs(i+1, j, curString,  m) or dfs(i, j+1, curString,  m) or dfs(i, j-1, curString, m): return True
+                visited.remove((i,j))
+            return False
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, "", 0):
+                    return True
+        return False
+            
+            
+            
