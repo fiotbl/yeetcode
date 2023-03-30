29a99719067fa829a062b3f305a57112b4929235
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-03-30 22:09:53 +0800

    Time: 446 ms (89.52%), Space: 15.7 MB (60.23%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2 == 1: return False
+        total = sum(nums)
+        if total % 2 == 1: return False
         target = sum(nums) // 2
         
         sums = set()
@@ -9,12 +10,8 @@ class Solution:
         for num in nums:
             tmp = set()
             for t in sums:
-                if (t+num) == target: return True
+                if num + t == target: return True
                 tmp.add(t)
                 tmp.add(t+num)
             sums = tmp
-        
         return False
-            
-
-                
