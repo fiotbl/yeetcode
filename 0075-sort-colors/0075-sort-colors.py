bf0a8db8c3411bbf803c67b241439c6805e79d0b
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-04-02 20:37:32 +0800

    Time: 30 ms (89.54%), Space: 13.9 MB (50.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,18 +5,19 @@ class Solution:
         """
         l, r = 0, len(nums)-1
         i = 0
-        def swap(i, j):
-            tmp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = tmp
         
-        while i <= r:
-            if nums[i] == 0:
-                swap(i, l)
-                l += 1
-            elif nums[i] == 2:
-                swap(i, r)
+        while i<=r:
+            if nums[i] == 2:
+                tmp = nums[r] 
+                nums[r] = 2
+                nums[i] = tmp
                 r -= 1
                 i -= 1
+            elif nums[i] == 0:
+                tmp = nums[l] 
+                nums[l] = 0
+                nums[i] = tmp
+                l += 1
             i += 1
+            
             
