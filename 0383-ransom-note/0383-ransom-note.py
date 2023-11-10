75ee859c5993f566024ee64f8ce0ca80325b8ad7
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-11-10 22:40:22 +0800

    Time: 56 ms (71.78%), Space: 16.5 MB (33.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mDict = {e:magazine.count(e) for e in set(magazine)}
-        
-        for letter in ransomNote:
-            if letter in mDict and mDict[letter]!=0:
-                mDict[letter] -= 1
-            else:
-                return False
-        
+        dic = {e:magazine.count(e) for e in set(magazine)}
+        for i in ransomNote:
+            if i not in dic: return False
+            elif dic[i] == 0: return False
+            else: dic[i] = dic[i] - 1
+                
         return True
