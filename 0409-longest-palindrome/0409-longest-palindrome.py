1115b937973a4136294bd3ac13b7b322270e14c7
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-11-11 22:26:09 +0800

    Time: 42 ms (47.96%), Space: 16.2 MB (79.57%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,13 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        counts = {e:s.count(e) for e in set(s)}
+        dic = {e:s.count(e) for e in set(s)}
         count = 0
         
-        for key in counts:
-            if counts[key] >= 2:
-                count += 2*(counts[key] // 2)
-                counts[key] %= 2
-                
-        if 1 in counts.values(): count += 1
+        for n in dic:
+            count += (dic[n]//2) * 2
+            dic[n] = dic[n] - (dic[n]//2) * 2
+        print(dic)
             
+        if 1 in dic.values(): count += 1
+        print(dic)
+        
         return count
