381b9f6e66544e9af0b0327c6bd45ea867096c31
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-11-11 23:46:38 +0800

    Time: 146 ms (78.09%), Space: 17.9 MB (33.40%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,12 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-                # boyer - moore algorithm
-        
-        count, res = 0, 0
+        count = 0
         
         for n in nums:
             if count == 0:
                 res = n
-            count += 1 if res == n else -1
-            
+                count += 1
+            elif n != res:
+                count -= 1
+            elif n == res:
+                count +=1 
+        
         return res
