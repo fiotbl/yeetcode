0e2d35d241f48d8fb3cc9af9d57c5c5a905ba3a0
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-11-13 19:09:28 +0800

    Time: 586 ms (41.10%), Space: 21.5 MB (6.13%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -3,34 +3,28 @@ class Solution:
         for i in range(len(mat)):
             for j in range(len(mat[0])):
                 if mat[i][j] == 0: continue
-                if j > 0:
-                    left = mat[i][j-1]
-                else:
-                    left = 10**999
                 if i > 0:
                     up = mat[i-1][j]
                 else:
                     up = 10**999
-                mat[i][j] = min(left, up) + 1
-
-        for i in range(len(mat) - 1, -1, -1):
-            for j in range(len(mat[0]) - 1, -1, -1):
-                if mat[i][j] == 0: continue
-                if j < len(mat[0]) - 1:
-                    right = mat[i][j+1]
+                if j > 0:
+                    left = mat[i][j-1]
                 else:
-                    right = 10**999
+                    left = 10**999
+                mat[i][j] = min(up, left) + 1
+                
+        for i in range(len(mat)-1, -1, -1):
+            for j in range(len(mat[0])-1, -1, -1):
+                if mat[i][j] == 0: continue
                 if i < len(mat) - 1:
                     down = mat[i+1][j]
                 else:
                     down = 10**999
-                value = min(down, right) + 1
-                mat[i][j] = min(value, mat[i][j]) 
-        
+                if j < len(mat[0]) - 1:
+                    right = mat[i][j+1]
+                else:
+                    right = 10**999
+                tmp = min(down, right) + 1
+                mat[i][j] = min(tmp, mat[i][j])
+                
         return mat
-                
-                
-        
-        
-                
-            
