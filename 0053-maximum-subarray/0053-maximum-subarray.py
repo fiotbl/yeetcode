14f5cd8e5ca54468ecb7df86ab17a482e3bc8b23
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-11-13 15:24:02 +0800

    Time: 583 ms (78.37%), Space: 30.5 MB (16.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,7 +5,7 @@ class Solution:
         if len(nums) == 1: return nums[0]
         for num in nums:
             total += num
-            if total > res: res = total
+            res = max(res, total)
             if total < 0: total = 0
             
         return res
