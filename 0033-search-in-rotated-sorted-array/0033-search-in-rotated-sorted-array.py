7c41d9c500044fd4d73db441be7dc063212fe911
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-11-22 21:33:01 +0800

    Time: 40 ms (92.07%), Space: 16.8 MB (11.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,22 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-        
+        l, r = 0, len(nums)-1
         while l<=r:
-            mid = (l+r) // 2
+            mid = (l+r)//2
             if nums[mid] == target: return mid
-            # check which is the strictly increasing side
             
-            if nums[mid] >= nums[l]:
-                if nums[mid] >= target and nums[l] <= target:
-                    r = mid - 1
+            # left side is increasing
+            if nums[mid] < nums[r]:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid -1
                 else:
-                    l = mid + 1
+                    l = mid +1
+                    
             else:
-                if nums[mid] <= target and nums[r] >= target:
+                if target > nums[mid] or target < nums[l]:
                     l = mid + 1
                 else:
                     r = mid - 1
         
         return -1
-                
