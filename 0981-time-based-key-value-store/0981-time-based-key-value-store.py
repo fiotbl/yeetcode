a31d253528e4535d1cfa97977e820327b72470c0
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2023-11-22 22:13:04 +0800

    Time: 666 ms (65.56%), Space: 74.1 MB (46.78%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,29 +1,29 @@
 class TimeMap:
 
     def __init__(self):
-        self.store = {}
-
+        self.dic = collections.defaultdict(list)
+        
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = [[value, timestamp]]
+        if key in self.dic:
+            self.dic[key].append((value, timestamp))
         else:
-            self.store[key].append([value, timestamp])
+            self.dic[key] = [(value, timestamp)]
 
     def get(self, key: str, timestamp: int) -> str:
-        values = self.store.get(key, [])
-        res = ""
+        if key not in self.dic: return ""
+        values = self.dic[key]
         
         l, r = 0, len(values)-1
-        
-        while l<=r:
-            mid = (l+r) // 2
-            if values[mid][1] <= timestamp:
+        res = ""
+        while l<= r:
+            mid = (l+r)//2
+            if values[mid][1] > timestamp:
+                r = mid -1
+            else:
                 res = values[mid][0]
                 l = mid + 1
-            else:
-                r = mid - 1
+                
         return res
-        
 
 
 # Your TimeMap object will be instantiated and called as such:
