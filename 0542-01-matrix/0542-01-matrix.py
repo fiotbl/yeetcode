71ba40074d2389264a452e8e1381cbe0f3f89839
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-01-22 23:10:40 +0800

    Time: 544 ms (51.06%), Space: 21.9 MB (7.14%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -4,27 +4,27 @@ class Solution:
             for j in range(len(mat[0])):
                 if mat[i][j] == 0: continue
                 if i > 0:
-                    up = mat[i-1][j]
-                else:
-                    up = 10**999
-                if j > 0:
-                    left = mat[i][j-1]
+                    left = mat[i-1][j]
                 else:
                     left = 10**999
+                if j > 0:
+                    up = mat[i][j-1]
+                else:
+                    up = 10**999
                 mat[i][j] = min(up, left) + 1
-                
+        
         for i in range(len(mat)-1, -1, -1):
             for j in range(len(mat[0])-1, -1, -1):
                 if mat[i][j] == 0: continue
-                if i < len(mat) - 1:
-                    down = mat[i+1][j]
-                else:
-                    down = 10**999
-                if j < len(mat[0]) - 1:
-                    right = mat[i][j+1]
+                if i < len(mat)-1:
+                    right = mat[i+1][j]
                 else:
                     right = 10**999
-                tmp = min(down, right) + 1
-                mat[i][j] = min(tmp, mat[i][j])
-                
+                if j < len(mat[0]) - 1:
+                    down = mat[i][j+1]
+                else:
+                    down = 10**999
+                mat[i][j] = min(min(right, down) + 1, mat[i][j])
+        
         return mat
+                
