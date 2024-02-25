4845b31ce896cec3cbae1f11959fb8e4ff8cc2e4
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-02-25 22:38:48 +0800

    Time: 4740 ms (37.30%), Space: 16.5 MB (83.00%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def search(i, j, m, visited):
-            if m == len(word):return True
-            if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or (i,j) in visited or word[m]!=board[i][j]: return False
-
+        def dfs(i, j, index, visited):
+            if index == len(word):
+                return True
+            if i<0 or i>len(board)-1: return False
+            if j<0 or j>len(board[0])-1: return False
+            if board[i][j] != word[index]: return False
+            if (i,j) in visited: return False
+            
             visited.add((i,j))
-            res = (search(i-1,j,m+1,visited) or search(i+1,j,m+1,visited) or search(i,j+1,m+1,visited) or search(i,j-1,m+1,visited))
+            res = dfs(i,j-1,index+1,visited) or dfs(i-1,j,index+1,visited) or dfs(i+1,j,index+1,visited) or dfs(i,j+1,index+1,visited)
             visited.remove((i,j))
-                
+            
             return res
-        
         
         visited = set()
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    if search(i,j,0,visited): return True
+                    if dfs(i,j,0,visited): return True
                 
         return False
-            
-            
-            
