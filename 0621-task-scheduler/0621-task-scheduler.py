a5c325f8d3a5cd6d2288e6f794783c48431856f6
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-04-17 21:37:21 +0800

    Time: 475 ms (27.43%), Space: 17.1 MB (32.69%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,21 +1,20 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         queue = []
-        tasksDict = {e:tasks.count(e) for e in set(tasks)}
-        counts = [-count for count in tasksDict.values()]
-        heapq.heapify(counts)
-        
+        counts = Counter(tasks)
+        heap1 = [-count for count in counts.values()]
+        heapq.heapify(heap1)
         time = 0
-        
-        while counts or queue:
+
+        while queue or heap1:
             time += 1
             
-            if counts:
-                cnt = 1 + heapq.heappop(counts)
+            if heap1:
+                cnt = 1 + heapq.heappop(heap1)
                 if cnt < 0:
-                    queue.append((cnt, time+n))
+                    queue.append([cnt, time + n])
             
             if queue and time == queue[0][1]:
-                heapq.heappush(counts, queue.pop(0)[0])
-                
+                heapq.heappush(heap1, queue.pop(0)[0])
+        
         return time
