5bbb23e9f34da839a4d446f5caa54e86eaee5e89
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-04-25 22:48:59 +0800

    Time: 769 ms (5.16%), Space: 17.6 MB (40.74%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        res = 0
+        visited = []
+        
+        def bfs(i,j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0 or (i,j) in visited: return 0
+            
+            visited.append((i,j))
+            
+            return 1 + bfs(i-1,j) + bfs(i+1,j) + bfs(i,j-1) + bfs(i, j+1)
+        
+        area = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    area = bfs(i,j)
+                res = max(res, area)
+        
+        return res
