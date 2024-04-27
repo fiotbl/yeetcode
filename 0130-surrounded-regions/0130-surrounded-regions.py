5eddc0cff1effcbf2291737766c7bf6fb2a5daf8
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-04-27 22:58:13 +0800

    Time: 115 ms (82.16%), Space: 17.9 MB (55.23%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def capture(r, c):
+            if r < 0 or c < 0 or r == len(board) or c == len(board[0]) or board[r][c] != "O":
+                return
+            
+            board[r][c] = "T"
+            capture(r-1, c)
+            capture(r+1, c)
+            capture(r, c-1)
+            capture(r, c+1)
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "O" and (i in [0, len(board)-1] or j in [0, len(board[0]) - 1]):
+                    capture(i,j)
+        
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "T":
+                    board[i][j] = "O"
