d30237d827e35fec1470605569bcfbd63cb6cb6e
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-04-29 22:05:32 +0800

    Time: 959 ms (42.97%), Space: 51.9 MB (25.14%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -10,12 +10,13 @@ class Solution:
         connections1 = { (i,j) for i,j in connections}
         visited = set()
         def dfs(i):
+            visited.add(i)
             for n in maps[i]:
                 if n in visited: continue
                 if (n,i) not in connections1:
                     nonlocal res
                     res +=1
-                visited.add(i)
+                
                 dfs(n)
         visited.add(0)
         dfs(0)
