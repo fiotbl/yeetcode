f70be0cbfb641b9eaddde180c6c09280c7197416
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-04-29 15:14:42 +0800

    Time: 532 ms (5.01%), Space: 17.6 MB (9.14%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        maps = {i:[] for i in range(1, len(edges)+1)}
+        
+        for i, j in edges:
+            maps[i].append(j)
+            maps[j].append(i)
+        
+    
+        def isTree(i, prev):
+            
+            if i in visited: return False
+            
+            visited.add(i)
+            for j in maps[i]:
+                if j == prev: continue
+                if not isTree(j, i): return False
+            
+            return True
+        
+        res = []
+        
+        for i, j in edges:
+            maps[i].remove(j)
+            maps[j].remove(i)
+            visited = set()
+            if isTree(1, -1) and len(visited) == len(edges): 
+                res = [i,j]
+            maps[i].append(j)
+            maps[j].append(i)
+        
+        return res
