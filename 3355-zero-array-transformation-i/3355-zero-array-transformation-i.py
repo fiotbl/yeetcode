415016d49719d2c72349c2e4718afe4f53ae8656
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-12-01 17:47:47 +0800

    Time: 107 ms (33.07%), Space: 55.3 MB (46.15%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.py
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.py
@@ -1,15 +1,16 @@
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        # Line sweep
         track = [0] * (len(nums) + 1)
         
-        for query in queries:
+        for query in queries:  # O(Q)
             track[query[0]] += 1
             track[query[1] + 1] -= 1
 
-        for j in range(1, len(track)):
+        for j in range(1, len(track)):  # O(n)
             track[j] += track[j-1]
 
-        for i, num in enumerate(nums):
+        for i, num in enumerate(nums): # O(n)
             if track[i] < num:
                 return False
             
