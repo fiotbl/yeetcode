889d7c779a3d0a3795777ea0e959afe2ab30af7b
Fiora Tan Bei Lun <42858864+fiotbl@users.noreply.github.com>
2024-12-02 19:56:52 +0800

    Time: 1205 ms (33.35%), Space: 63.8 MB (11.23%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
@@ -1,10 +1,11 @@
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-
+        if all(num == 0 for num in nums):
+            return 0
         
         def find(target):
             track = [0] * (len(nums) + 1)
-            for query in queries[:target]:  # O(Q)
+            for query in queries[:target+1]:  # O(Q)
                 track[query[0]] += query[2]
                 track[query[1] + 1] -= query[2]
 
@@ -17,14 +18,15 @@ class Solution:
 
             return True
         
-        l, r = 0, len(queries) + 1
-        
+        l, r = 0, len(queries) 
+        res = 0
         while l<=r:
             mid = (l+r)//2
             if find(mid):
+                res = mid
                 r = mid - 1
             else:
                 l = mid + 1
                 
         if l > len(queries): return -1
-        return l
+        return l+1
